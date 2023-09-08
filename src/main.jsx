ba928e1ca9373368621dd79af20de20e9207cc90
thanhit08Tekville <thanhit08@tekville.com>
2023-09-08 16:22:38 +0900

    fix: vite log

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from 'https://<your_github_username>.github.io/App.jsx'
+import 'https://<your_github_username>.github.io/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
