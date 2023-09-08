b3b9594ea6253b0a0237f92040dc037a10cd0758
thanhit08Tekville <thanhit08@tekville.com>
2023-09-08 17:33:47 +0900

    add: environment

--- src/VRApp.jsx
+++ src/VRApp.jsx
@@ -1,13 +1,16 @@
 import { VRButton, ARButton, XR, Controllers, Hands, RayGrab } from '@react-three/xr'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import Polyhedron from './Polyhedron.jsx'
+import { RGBELoader } from 'three-stdlib'
+import { PMREMGenerator, Texture } from 'three'
 import * as THREE from 'three'
-import { useMemo } from 'react'
-import { 
-    Stats, 
-    OrbitControls, 
+import { useMemo, useEffect, useState } from 'react'
+import {
+    Stats,
+    OrbitControls,
     ContactShadows,
-    Environment } from '@react-three/drei'
+    Environment
+} from '@react-three/drei'
 import { Leva, useControls } from 'leva'
 
 
@@ -33,48 +36,74 @@ const options = {
     model: { value: 'Box', options: Object.keys(my_polyhedron) }
 }
 
+function ControllersWithEnvMap() {
+    const renderer = useThree(({ gl }) => gl);
+    const [envMap, setEnvMap] = useState(null); // Initialize with null instead of specifying a type
+
+    useEffect(() => {
+        const generateEnvMap = async () => {
+            const rgbeLoader = new RGBELoader();
+            const dataTexture = await rgbeLoader.loadAsync('./resting.hdr');
+            const pmremGenerator = new PMREMGenerator(renderer);
+            pmremGenerator.compileEquirectangularShader();
+            const rt = pmremGenerator.fromEquirectangular(dataTexture);
+            const radianceMap = rt.texture;
+            setEnvMap(radianceMap);
+            pmremGenerator.dispose();
+        };
+
+        generateEnvMap();
+    }, [renderer]);
+
+    return <Controllers envMap={envMap} envMapIntensity={1} />;
+}
+
+
 export default function VRApp() {
     const pA = useControls('Polyhedron A', options)
     const pB = useControls('Polyhedron B', options)
+
     return (
         <>
             <VRButton />
-            <Canvas camera={{ position: [1, 2, 3] }} >
-                <XR>
-                    <Controllers />
-                    <Hands />
-                    <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
-                    <spotLight position={[50, 50, 10]} angle={0.15} penumbra={1} />
-                    <RayGrab>
-                        <Polyhedron
-                            key="polyhedronA"
-                            position={[-1, 1, 0]}
-                            rotation={[pA.x, pA.y, pA.z]}
-                            visible={pA.visible}
-                            color={pA.color}
-                            wireframe={pA.wireframe}
-                            polyhedron_shape={my_polyhedron[pA.model]}
-                        />
-                    </RayGrab>
-                    <RayGrab>
-                        <Polyhedron
-                            key="polyhedronB"
-                            position={[1, 1, 0]}
-                            rotation={[pB.x, pB.y, pB.z]}
-                            visible={pB.visible}
-                            color={pB.color}
-                            wireframe={pB.wireframe}
-                            polyhedron_shape={my_polyhedron[pB.model]}
-                        />
-                    </RayGrab>
-                    <ContactShadows scale={20} blur={10} far={20} />
-                    <Environment files="./resting.hdr" background />
-                    <OrbitControls target-y={1} />
-                    <axesHelper args={[5]} />
-                    <gridHelper />
-                    <Stats />
-                </XR>
-            </Canvas>
+            <div style={{ width: "50vw", height: "50vh" }}>
+                <Canvas camera={{ position: [-10, 10, 10], fov: 50 }}>
+                    <XR>
+                        <ControllersWithEnvMap />
+                        <Hands />
+                        <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
+                        <spotLight position={[50, 50, 10]} angle={0.15} penumbra={1} />
+                        <RayGrab>
+                            <Polyhedron
+                                key="polyhedronA"
+                                position={[-5, 5, 5]}
+                                rotation={[pA.x, pA.y, pA.z]}
+                                visible={pA.visible}
+                                color={pA.color}
+                                wireframe={pA.wireframe}
+                                polyhedron_shape={my_polyhedron[pA.model]}
+                            />
+                        </RayGrab>
+                        <RayGrab>
+                            <Polyhedron
+                                key="polyhedronB"
+                                position={[5, 5, 5]}
+                                rotation={[pB.x, pB.y, pB.z]}
+                                visible={pB.visible}
+                                color={pB.color}
+                                wireframe={pB.wireframe}
+                                polyhedron_shape={my_polyhedron[pB.model]}
+                            />
+                        </RayGrab>
+                        <ContactShadows scale={20} blur={10} far={20} />
+                        <Environment files="./resting.hdr" background />
+                        <OrbitControls target-y={1} />
+                        <axesHelper args={[5]} />
+                        <gridHelper />
+                        <Stats />
+                    </XR>
+                </Canvas>
+            </div>
         </>
     )
 }
