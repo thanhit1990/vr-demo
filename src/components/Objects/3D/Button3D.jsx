ba041394c01808df030ab2439d53e16a3ac7be14
W21H2-10\user <thanhit08@msn.com>
2023-09-11 20:38:42 +0900

    Close tab from VR

--- src/components/Objects/3D/Button3D.jsx
+++ src/components/Objects/3D/Button3D.jsx
@@ -1,6 +1,6 @@
-import {Interactive} from '@react-three/xr'
-import {useState} from 'react'
-import {Text} from '@react-three/drei'
+import { Interactive } from '@react-three/xr'
+import { useRef, useState } from 'react'
+import { Text } from '@react-three/drei'
 
 function Box({ color, size, scale, children, ...rest }) {
     return (
@@ -15,6 +15,7 @@ function Box({ color, size, scale, children, ...rest }) {
 export default function Button3D(props) {
     const [hover, setHover] = useState(false);
     const [color, setColor] = useState(0x123456);
+    const ref = useRef()
     const onSelect = () => {
         setColor((Math.random() * 0xffffff) | 0);
         if (window.opener) {
@@ -24,12 +25,24 @@ export default function Button3D(props) {
     };
 
     return (
-        <Interactive onSelect={onSelect} onHover={() => setHover(true)} onBlur={() => setHover(false)}>
-            <Box color={color} scale={hover ? [7.5, 7.5, 7.5] : [5, 5, 5]} size={[0.4, 0.1, 0.1]} {...props}>
-                <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
-                    Exit Room!
-                </Text>
-            </Box>
-        </Interactive>
+        <mesh
+            ref={ref}
+            onPointerDown={() => {
+                console.log('click')
+                if (window.opener) {
+                    // window.opener.location.href = '/tab-a';
+                    window.close(); // Close tab B
+                }
+            }}
+        >
+            <Interactive onSelect={onSelect} onHover={() => setHover(true)} onBlur={() => setHover(false)}>
+                <Box color={color} scale={hover ? [7.5, 7.5, 7.5] : [5, 5, 5]} size={[0.4, 0.1, 0.1]} {...props}>
+                    <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
+                        Exit Room!
+                    </Text>
+                </Box>
+            </Interactive>
+        </mesh>
+
     );
 }
